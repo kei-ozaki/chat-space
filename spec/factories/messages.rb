cf02a2a760262317ba2de42634440d00f895ab46
kei-ozaki <nya33n@gmail.com>
2018-04-07 15:53:47 +0900

    messages model test

--- spec/factories/messages.rb
+++ spec/factories/messages.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
   factory :message do
-    text Faker::Lorem.sentence
-    image File.open("#{Rails.root}/public/images/no_image.jpg")
+    text      Faker::Lorem.sentence
+    image     File.open("#{Rails.root}/spec/fixtures/picture.png")
     user
     group
   end
