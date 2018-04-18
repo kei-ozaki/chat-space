945cd6355e0d0775cd8f1dc33c30182934b9ec57
kei-ozaki <nya33n@gmail.com>
2018-04-18 19:07:17 +0900

    js file add HTML
    jsファイルにaAjaxが動いた時のhtmlを書いた

--- app/assets/javascripts/user_search.js
+++ app/assets/javascripts/user_search.js
@@ -1,12 +1,26 @@
 $(function(){
 
-  function appendUser(user){
+  var searchList = $("#user-search-result");
 
-  }
+  function appendUser(user){
+    var html =  `
+                <div class="chat-group-user clearfix">
+                  <p class="chat-group-user__name">${user.name}</p>
+                  <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加</a>
+                </div>
+                `
+    // console.log(html);
+    searchList.append(html);
+  };
 
   function appendNoUser(user){
-
-  }
+    var html =  `
+                <div class="chat-group-user clearfix">
+                  <p class="chat-group-user__name">${user}</p>
+                </div>
+                `
+    searchList.append(html);
+  };
 
   $("#user-search-field").on("keyup", function(){
     var input = $("#user-search-field").val();
@@ -19,7 +33,7 @@ $(function(){
       dataType: 'json'
     })
     .done(function(users){
-      $(".user-search-result").empty();
+      searchList.empty();
       if (users.length !== 0){
         users.forEach(function(user){
           appendUser(user);
@@ -27,7 +41,7 @@ $(function(){
       }
       else {
         appendNoUser("一致するユーザーがいません");
-      }
+      };
     })
     .fail(function(){
       alert("ユーザー検索に失敗しました")
