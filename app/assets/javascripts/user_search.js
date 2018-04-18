1fd845c5b87b835039c18bc36c9000721768264e
kei-ozaki <nya33n@gmail.com>
2018-04-18 13:51:29 +0900

    jsファイル記入
    doneとappendUser,appendNoUserの分岐まで書いて一旦コミットした

--- app/assets/javascripts/user_search.js
+++ app/assets/javascripts/user_search.js
@@ -1,5 +1,36 @@
 $(function(){
-  $(".user-search-field").on("keyup", function(){
-    var input = $(".user-search-field").val();
-  })
-})
+
+  function appendUser(user){
+
+  }
+
+  function appendNoUser(user){
+
+  }
+
+  $("#user-search-field").on("keyup", function(){
+    var input = $("#user-search-field").val();
+    // console.log(input);
+
+    $.ajax({
+      type: 'GET',
+      url: '/users',
+      data: { keyword: input },
+      dataType: 'json'
+    })
+    .done(function(users){
+      $(".user-search-result").empty();
+      if (users.length !== 0){
+        users.forEach(function(user){
+          appendUser(user);
+        });
+      }
+      else {
+        appendNoUser("一致するユーザーがいません");
+      }
+    })
+    .fail(function(){
+      alert("ユーザー検索に失敗しました")
+    })
+  });
+});
