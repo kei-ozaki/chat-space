41241a765494221c614133a599a58c9852de5cb6
kei-ozaki <nya33n@gmail.com>
2018-04-19 13:08:57 +0900

    入力するまでメンバーが出ないようにした
    最初からメンバー全員出てしまっていたので入力されるまでは一覧が出ないようにした

--- app/assets/javascripts/user_search.js
+++ app/assets/javascripts/user_search.js
@@ -22,26 +22,28 @@ $(function(){
 
   $("#user-search-field").on("keyup", function(){
     var input = $("#user-search-field").val();
-    $.ajax({
-      type: 'GET',
-      url: '/users',
-      data: { keyword: input },
-      dataType: 'json'
-    })
-    .done(function(users){
-      usResult.empty();
-      if (users.length !== 0){
-        users.forEach(function(user){
-          appendUser(user);
-        });
-      }
-      else {
-        appendNoUser("一致するユーザーがいません");
-      };
-    })
-    .fail(function(){
-      alert("ユーザー検索に失敗しました")
-    })
+    if (input.length !== 0) {
+      $.ajax({
+        type: 'GET',
+        url: '/users',
+        data: { keyword: input },
+        dataType: 'json'
+      })
+      .done(function(users){
+        usResult.empty();
+        if (users.length !== 0){
+          users.forEach(function(user){
+            appendUser(user);
+          });
+        }
+        else {
+          appendNoUser("一致するユーザーがいません");
+        };
+      })
+      .fail(function(){
+        alert("ユーザー検索に失敗しました")
+      })
+    }
   });
 
   function addUser(userId, userName){
